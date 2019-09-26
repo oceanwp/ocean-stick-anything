2a5c1cdb2c6478f914e785b5e3dfe111076319cb
Alex de Borba <mail@alexdeborba.com>
2019-09-26 10:36:00 -0500

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+var $j = jQuery.noConflict();
+
+$j( document ).on( 'ready', function() {
+	oceanwpStickAnything();
+} );
+
+// On resize
+$j( window ).resize( function() {
+	oceanwpStickAnything();
+} );
+
+/* ==============================================
+STICK ANYTHING
+============================================== */
+function oceanwpStickAnything() {
+	"use strict"
+
+	var $offset 		= 0,
+	    $stickOffset 	= $j( 'body' ).data( 'offset' ),
+	    $adminBar 		= $j( '#wpadminbar' ),
+	    $stickyTopBar 	= $j( '#top-bar-wrap' ),
+	    $stickyHeader 	= $j( '#site-header' );
+
+    // If custom offset
+    if ( $stickOffset ) {
+		$offset = $offset + $stickOffset;
+	}
+
+    // Offset adminbar
+    if ( $adminBar.length
+    	&& $j( window ).width() > 600 ) {
+		$offset = $offset + $adminBar.outerHeight();
+	}
+
+    // Offset sticky topbar
+    if ( true == oceanwpLocalize.hasStickyTopBar ) {
+		$offset = $offset + $stickyTopBar.outerHeight();
+	}
+
+    // Offset sticky header
+    if ( $j( '#site-header' ).hasClass( 'fixed-scroll' )
+    	&& ! $j( '#site-header' ).hasClass( 'vertical-header' ) ) {
+		$offset = $offset + $stickyHeader.outerHeight();
+	}
+
+	$j( oceanwpLocalize.stickElements ).stick_in_parent( {
+		offset_top: $offset,
+	} );
+
+	// If un-stick
+	if ( oceanwpLocalize.unStick
+    	&& $j( window ).width() <= oceanwpLocalize.unStick ) {
+		$j( oceanwpLocalize.stickElements ).trigger( 'sticky_kit:detach' );
+	}
+
+}
---
+++ readme.txt
@@ -0,0 +1,45 @@
+=== Ocean Stick Anything ===
+Contributors: oceanwp
+Tags: stick, sticky, stick anything, oceanwp
+Requires at least: 4.5
+Tested up to: 5.2
+Stable tag: 1.0.3
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+A simple plugin to stick anything you want.
+This plugin requires the [OceanWP](https://oceanwp.org/) theme to be installed.
+
+== Installation ==
+
+1. Upload `ocean-stick-anything` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Configure it via Theme Panel > Stick Elements
+4. Done!
+
+== Frequently Asked Questions ==
+
+= I installed the plugin but it does not work =
+
+This plugin will only work with the [OceanWP](https://oceanwp.org/) theme. You also need to install the free [Ocean Extra](https://wordpress.org/plugins/ocean-extra/) plugin
+
+== Screenshots ==
+
+1. Settings.
+
+== Changelog ==
+
+= 1.0.3 =
+- Added: Codes for the Freemius switch.
+- Fixed: Small error issue.
+
+= 1.0.2 =
+- Fixed: Extra bracket in the data offset in the body tag.
+
+= 1.0.1 =
+- Tweak: Sticky script improved, the sticky can be destroyed on window resize.
+
+= 1.0.0 =
+- Initial release.
