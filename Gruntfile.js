0203fe6cf638096240337fc5aff0f23f1d1bd1aa
web-devise <manager@devise.com.ua>
2022-05-06 16:39:59 +0300

    Version 2.0.4 - Theme Panel

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,189 +1,192 @@
-module.exports = function ( grunt ) {
+module.exports = function (grunt) {
+    const sass = require("node-sass");
 
-	const sass = require('node-sass');
+    // require it at the top and pass in the grunt instance
+    require("time-grunt")(grunt);
 
-	// require it at the top and pass in the grunt instance
-	require( 'time-grunt' )( grunt );
+    // Load all Grunt tasks
+    require("jit-grunt")(grunt, {
+        makepot: "grunt-wp-i18n",
+    });
 
-	// Load all Grunt tasks
-	require( 'jit-grunt' )( grunt, {
-		makepot: 'grunt-wp-i18n'
-	} );
+    grunt.initConfig({
+        pkg: grunt.file.readJSON("package.json"),
 
-	grunt.initConfig( {
+        browserify: {
+            dev: {
+                files: {
+                    "assets/js/stick-anythings.js": "assets/src/js/stick-anythings.js",
+                },
+                options: {
+                    transform: [["babelify", { presets: ["@babel/preset-env"] }]],
+                    browserifyOptions: {
+                        debug: true,
+                    },
+                },
+            },
+        },
 
-		pkg: grunt.file.readJSON( 'package.json' ),
+        // Concat and Minify our js.
+        uglify: {
+            prod: {
+                files: {
+                    "assets/js/stick-anythings.min.js": "assets/js/stick-anythings.js",
+                },
+            },
+        },
 
-		// Concat and Minify our js.
-		uglify: {
-			prod: {
-				files: {
-					'assets/js/main.min.js': 'assets/js/main.js'
-				}
-			}
-		},
+        // Minify CSS
+        cssmin: {
+            options: {
+                shorthandCompacting: false,
+                roundingPrecision: -1,
+                keepSpecialComments: 0,
+            },
+            prod: {
+                files: {
+                    "assets/css/main.min.css": "assets/css/main.css",
+                },
+            },
+        },
 
-		// Minify CSS
-		cssmin: {
-			options: {
-				shorthandCompacting: false,
-				roundingPrecision: -1,
-				keepSpecialComments: 0
-			},
-			prod: {
-				files: {
-					'assets/css/main.min.css': 'assets/css/main.css',
-				}
-			}
-		},
+        // Autoprefixer.
+        autoprefixer: {
+            options: {
+                browsers: ["last 8 versions", "ie 8", "ie 9"],
+            },
+            main: {
+                files: {
+                    "assets/css/style.css": "assets/css/style.css",
+                    "assets/css/style.min.css": "assets/css/style.min.css",
+                },
+            },
+        },
 
-		// Autoprefixer.
-		autoprefixer: {
-			options: {
-				browsers: [
-					'last 8 versions', 'ie 8', 'ie 9'
-				]
-			},
-			main: {
-				files: {
-					'assets/css/style.css': 'assets/css/style.css',
-					'assets/css/style.min.css': 'assets/css/style.min.css',
-				}
-			}
-		},
+        // Sorting our CSS properties.
+        csscomb: {
+            options: {
+                config: ".csscomb.json",
+            },
+            main: {
+                files: {
+                    "assets/css/style.css": ["assets/css/style.css"],
+                },
+            },
+        },
 
-		// Sorting our CSS properties.
-		csscomb: {
-			options: {
-				config: '.csscomb.json'
-			},
-			main: {
-				files: {
-					'assets/css/style.css': [ 'assets/css/style.css' ]
-				}
-			}
-		},
+        // Newer files checker
+        newer: {
+            options: {
+                override: function (detail, include) {
+                    if (detail.task === "php" || detail.task === "sass") {
+                        include(true);
+                    } else {
+                        include(false);
+                    }
+                },
+            },
+        },
 
-		// Newer files checker
-		newer: {
-			options: {
-				override: function ( detail, include ) {
-					if ( detail.task === 'php' || detail.task === 'sass' ) {
-						include( true );
-					} else {
-						include( false );
-					}
-				}
-			}
-		},
+        // Watch for changes.
+        watch: {
+            options: {
+                livereload: 35733,
+                livereloadOnError: false,
+                spawn: false,
+            },
+            scss: {
+                files: ["assets/css/**/*.scss"],
+                tasks: ["newer:autoprefixer:main"],
+            },
+            js: {
+                files: ["assets/src/**/**.js"],
+                tasks: ["newer:browserify:dev", "newer:uglify:prod"],
+            },
+        },
 
-		// Watch for changes.
-		watch: {
-			options: {
-				livereload: true,
-				spawn: false
-			},
-			scss: {
-				files: [ 'assets/css/**/*.scss' ],
-				tasks: [
-					'newer:sass:dev',
-					'newer:autoprefixer:main',
-				]
-			}
-		},
+        // Copy the theme into the build directory
+        copy: {
+            build: {
+                expand: true,
+                src: [
+                    "**",
+                    "!node_modules/**",
+                    "!build/**",
+                    "!.git/**",
+                    "!changelog.txt",
+                    "!Gruntfile.js",
+                    "!package.json",
+                    "!package-lock.json",
+                    "!.csscomb.json",
+                    "!.tern-project",
+                    "!.gitignore",
+                    "!.jshintrc",
+                    "!.DS_Store",
+                    "!*.map",
+                    "!**/*.map",
+                    "!**/Gruntfile.js",
+                    "!**/package.json",
+                    "!**/*~",
+                ],
+                dest: "build/<%= pkg.name %>/",
+            },
+        },
 
-		// Copy the theme into the build directory
-		copy: {
-			build: {
-				expand: true,
-				src: [
-					'**',
-					'!node_modules/**',
-					'!build/**',
-					'!.git/**',
-					'!changelog.txt',
-					'!Gruntfile.js',
-					'!package.json',
-					'!package-lock.json',
-					'!.csscomb.json',
-					'!.tern-project',
-					'!.gitignore',
-					'!.jshintrc',
-					'!.DS_Store',
-					'!*.map',
-					'!**/*.map',
-					'!**/Gruntfile.js',
-					'!**/package.json',
-					'!**/*~'
-				],
-				dest: 'build/<%= pkg.name %>/'
-			}
-		},
+        // Compress build directory into <name>.zip
+        compress: {
+            build: {
+                options: {
+                    mode: "zip",
+                    archive: "./build/<%= pkg.name %>.zip",
+                },
+                expand: true,
+                cwd: "build/<%= pkg.name %>/",
+                src: ["**/*"],
+                dest: "<%= pkg.name %>/",
+            },
+        },
 
-		// Compress build directory into <name>.zip
-		compress: {
-			build: {
-				options: {
-					mode: 'zip',
-					archive: './build/<%= pkg.name %>.zip'
-				},
-				expand: true,
-				cwd: 'build/<%= pkg.name %>/',
-				src: [ '**/*' ],
-				dest: '<%= pkg.name %>/'
-			}
-		},
+        makepot: {
+            target: {
+                options: {
+                    domainPath: "/languages/", // Where to save the POT file.
+                    exclude: [
+                        // Exlude folder.
+                        "build/.*",
+                        "assets/.*",
+                        "readme/.*",
+                        "sass/.*",
+                        "bower_components/.*",
+                        "node_modules/.*",
+                    ],
+                    potFilename: "<%= pkg.name %>.pot", // Name of the POT file.
+                    type: "wp-plugin", // Type of project (wp-plugin or wp-theme).
+                    updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
+                    processPot: function (pot, options) {
+                        pot.headers["plural-forms"] = "nplurals=2; plural=n != 1;";
+                        pot.headers["last-translator"] = "OceanWP\n";
+                        pot.headers["language-team"] = "OceanWP\n";
+                        pot.headers["x-poedit-basepath"] = "..\n";
+                        pot.headers["x-poedit-language"] = "English\n";
+                        pot.headers["x-poedit-country"] = "UNITED STATES\n";
+                        pot.headers["x-poedit-sourcecharset"] = "utf-8\n";
+                        pot.headers["x-poedit-searchpath-0"] = ".\n";
+                        pot.headers["x-poedit-keywordslist"] =
+                            "_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n";
+                        pot.headers["x-textdomain-support"] = "yes\n";
+                        return pot;
+                    },
+                },
+            },
+        },
+    });
 
-		makepot: {
-			target: {
-				options: {
-					domainPath: '/languages/', // Where to save the POT file.
-					exclude: [ // Exlude folder.
-						'build/.*',
-						'assets/.*',
-						'readme/.*',
-						'sass/.*',
-						'bower_components/.*',
-						'node_modules/.*'
-					],
-					potFilename: '<%= pkg.name %>.pot', // Name of the POT file.
-					type: 'wp-plugin', // Type of project (wp-plugin or wp-theme).
-					updateTimestamp: true, // Whether the POT-Creation-Date should be updated without other changes.
-					processPot: function ( pot, options ) {
-						pot.headers[ 'plural-forms' ] = 'nplurals=2; plural=n != 1;';
-						pot.headers[ 'last-translator' ] = 'OceanWP\n';
-						pot.headers[ 'language-team' ] = 'OceanWP\n';
-						pot.headers[ 'x-poedit-basepath' ] = '..\n';
-						pot.headers[ 'x-poedit-language' ] = 'English\n';
-						pot.headers[ 'x-poedit-country' ] = 'UNITED STATES\n';
-						pot.headers[ 'x-poedit-sourcecharset' ] = 'utf-8\n';
-						pot.headers[ 'x-poedit-searchpath-0' ] = '.\n';
-						pot.headers[ 'x-poedit-keywordslist' ] = '_esc_attr__;esc_attr_x;esc_attr_e;esc_html__;esc_html_e;esc_html_x;__;_e;__ngettext:1,2;_n:1,2;__ngettext_noop:1,2;_n_noop:1,2;_c;_nc:4c,1,2;_x:1,2c;_ex:1,2c;_nx:4c,1,2;_nx_noop:4c,1,2;\n';
-						pot.headers[ 'x-textdomain-support' ] = 'yes\n';
-						return pot;
-					}
-				}
-			}
-		}
+    // Dev task
+    grunt.registerTask("default", ["browserify:dev", "uglify:prod", "autoprefixer:main", "cssmin:prod"]);
 
-	} );
+    // Production task
+    grunt.registerTask("build", ["copy"]);
 
-	// Dev task
-	grunt.registerTask( 'default', [
-		'cssmin:prod',
-	] );
-
-	// Production task
-	grunt.registerTask( 'build', [
-		'newer:uglify:prod',
-		'autoprefixer:main',
-		'csscomb:main',
-		'copy'
-	] );
-
-	// Package task
-	grunt.registerTask( 'package', [
-		'compress',
-	] );
-
+    // Package task
+    grunt.registerTask("package", ["compress"]);
 };
